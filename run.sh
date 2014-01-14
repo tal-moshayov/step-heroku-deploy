761ca6fe9d8a39629688a9439418910b8a3ec4cf
Jacco Flenter <github@putpixel.nl>
2014-01-14 18:09:26 +0100

    apt-get update added

--- run.sh
+++ run.sh
@@ -49,6 +49,7 @@ then
     cd $TMPDIR
     # result=$(sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh)
 
+    sudo apt-get update
     sudo apt-get install -y ruby1.9.1
     result=$(sudo dpkg -i $WERCKER_STEP_ROOT/foreman-0.60.0.deb $WERCKER_STEP_ROOT/heroku-3.2.0.deb $WERCKER_STEP_ROOT/heroku-toolbelt-3.2.0.deb)
 
